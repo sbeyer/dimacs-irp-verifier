c7eded0d52b6b3ea596991c69d0f655238a5a8d9
Stephan Beyer <s.beyer@slashwhy.de>
2020-12-12 00:17:49 +0100

    Add parsing of input file

--- verify.py
+++ verify.py
@@ -11,4 +11,57 @@
 # You should have received a copy of the CC0 Public Domain Dedication along with this
 # software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
-print("Runs!")
+
+def read_instance(fn):
+    lines = open(fn, "r").read().splitlines()
+
+    meta_data = lines.pop(0).split()
+    (num_nodes, num_days, capacity, num_vehicles) = [int(val) for val in meta_data]
+
+    pos = []
+    inventory_start = []
+    inventory_change = []
+    inventory_max = []
+    inventory_min = []
+    inventory_cost = []
+
+    depot_data = lines.pop(0).split()
+    (_, x, y, *depot_data) = depot_data
+    pos.append((float(x), float(y)))
+    inventory_start.append(int(depot_data.pop(0)))
+    inventory_change.append(int(depot_data.pop(0)))
+    inventory_cost.append(float(depot_data.pop(0)))
+    inventory_max.append(inventory_start[0] + inventory_change[0] * num_days)
+    inventory_min.append(0)
+
+    for line in lines:
+        customer_data = line.split()
+        (_, x, y, *customer_data) = customer_data
+        pos.append((float(x), float(y)))
+        inventory_start.append(int(customer_data.pop(0)))
+        inventory_max.append(int(customer_data.pop(0)))
+        inventory_min.append(int(customer_data.pop(0)))
+        inventory_change.append(-int(customer_data.pop(0)))
+        inventory_cost.append(float(customer_data.pop(0)))
+
+    print(f"Number of nodes: {num_nodes}")
+    print(f"Number of days: {num_days}")
+    print(f"Number of vehicles: {num_vehicles}")
+    print(f"Vehicle capacity: {capacity}")
+    print(f"Positions: {pos}")
+    print(f"Inventory start levels: {inventory_start}")
+    print(f"Inventory max levels: {inventory_max}")
+    print(f"Inventory min levels: {inventory_min}")
+    print(f"Inventory cost: {inventory_cost}")
+    print(f"Daily level change: {inventory_change}")
+
+
+fn_instance = "example/S_abs5n5_4_L3.dat"
+fn_solution = "example/out_S_abs5n5_4_L3.txt"
+
+read_instance(fn_instance)
+
+print("Solution:")
+lines_solution = open(fn_solution, "r").read().splitlines()
+for line in lines_solution:
+    print(line)
