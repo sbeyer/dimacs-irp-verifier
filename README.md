44ef949e3ea17dbf9a3c6a6fa7e45adf4ca33523
Stephan Beyer <s.beyer@slashwhy.de>
2020-12-11 23:15:20 +0100

    Initial commit with README, script stub and CC0 license

---
+++ README.md
@@ -0,0 +1,5 @@
+# dimacs-irp-verifier
+
+This is a Python script to verify solutions for the IRP track
+of the 12th DIMACS Implementation Challenge on Vehicle Routing Problems
+http://dimacs.rutgers.edu/programs/challenge/vrp/irp/
---
+++ verify.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#
+# DIMACS IRP track solution verifier
+#
+# Written in 2020 by Stephan Beyer <sbeyer@slashwhy.de>, slashwhy GmbH & Co. KG
+#
+# To the extent possible under law, the author(s) have dedicated all copyright and
+# related and neighboring rights to this software to the public domain worldwide. This
+# software is distributed without any warranty.
+#
+# You should have received a copy of the CC0 Public Domain Dedication along with this
+# software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+print("Runs!")
