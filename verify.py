d857800e53e59cae71ffc53f3c150ca4bb18754e
Stephan Beyer <sbeyer@slashwhy.de>
2021-10-11 02:02:25 +0200

    Update year in comment

--- verify.py
+++ verify.py
@@ -2,7 +2,7 @@
 #
 # DIMACS IRP track solution verifier
 #
-# Written in 2020 by Stephan Beyer <sbeyer@slashwhy.de>, slashwhy GmbH & Co. KG
+# Written in 2020 and 2021 by Stephan Beyer <sbeyer@slashwhy.de>, slashwhy GmbH & Co. KG
 #
 # To the extent possible under law, the author(s) have dedicated all copyright and
 # related and neighboring rights to this software to the public domain worldwide. This
